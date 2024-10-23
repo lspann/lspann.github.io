f63abff800c7b212d6a75eb78e7724e53f2dcbfb
lspann <elsp9468@colorado.edu>
2024-10-22 18:45:08 -0600

    Website Rehaul

--- aboutMe.js
+++ aboutMe.js
@@ -1,25 +1,3 @@
-//cursor
-const cursor = document.querySelector(".cursor");
-var timeout;
-
-document.addEventListener("mousemove", (e) => {
-    let x = e.pageX;
-    let y = e.pageY;
-
-    cursor.style.top = y + "px";
-    cursor.style.left = x + "px";
-    cursor.style.display = "block";
-
-    function mouseStopped(){
-        cursor.style.display = "none";
-    }
-    // clearTimeout(timeout);
-    // timeout = setTimeout(mouseStopped, 1000);
-});
-
-document.addEventListener("mouseout", () => {
-    cursor.style.display = "none";
-});
 
 //Navigation
 let toggleMenu = document.querySelector('.toggleMenu');
